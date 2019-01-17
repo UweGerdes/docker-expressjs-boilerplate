1c5c6b11650a65fdc7270ff8d69830ec1abbebdd
Uwe Gerdes <entwicklung@uwegerdes.de>
2019-01-17 22:28:18 +0100

    adding checkbox and radio with utf-8 style

--- modules/boilerplate/tests/views/form.js
+++ modules/boilerplate/tests/views/form.js
@@ -15,7 +15,7 @@ chai.use(chaiHttp);
 
 describe('/boilerplate/tests/views/form.js', function () {
   describe('GET /boilerplate/', function () {
-    it('should have head and script', function (done) {
+    it('should have head, script and headline', function (done) {
       chai.request('http://localhost:8080')
         .get('/boilerplate/form/')
         .end(function (err, res) {
@@ -29,10 +29,14 @@ describe('/boilerplate/tests/views/form.js', function () {
             document.head.getElementsByTagName('script')[0].attributes.src.nodeValue,
             '/js/boilerplate/script.js'
           );
+          const headline = document.getElementById('headline');
+          assert.equal(headline.textContent, 'Boilerplate');
+          const subHeadline = document.querySelector('.subheadline');
+          assert.equal(subHeadline.textContent, 'Formular');
           done();
         });
     });
-    it('should have headline', function (done) {
+    it('should have form and widgets', function (done) {
       chai.request('http://localhost:8080')
         .get('/boilerplate/form/')
         .end(function (err, res) {
@@ -40,8 +44,23 @@ describe('/boilerplate/tests/views/form.js', function () {
           expect(res).to.have.status(200);
           expect(res).to.be.html;
           const { document } = (new JSDOM(res.text)).window;
-          const headline = document.getElementById('headline');
-          assert.equal(headline.textContent, 'Boilerplate');
+          const form = document.querySelector('form');
+          assert.equal(form.getAttribute('method'), 'POST');
+          assert.equal(form.getAttribute('action'), '/boilerplate/form/');
+          const inputTextContainer = document.querySelector('.field_username');
+          assert.equal(inputTextContainer.textContent, 'Username');
+          const inputText = inputTextContainer.querySelector('.input-text');
+          assert.equal(inputText.name, 'username');
+          assert.equal(inputText.getAttribute('type'), 'text');
+          const inputPasswordContainer = document.querySelector('.field_password');
+          assert.equal(inputPasswordContainer.textContent, 'Passwort');
+          const inputPassword = inputPasswordContainer.querySelector('.input-password');
+          assert.equal(inputPassword.name, 'password');
+          assert.equal(inputPassword.getAttribute('type'), 'password');
+          const inputSubmitContainer = document.querySelector('.field_submit');
+          const inputSubmit = inputSubmitContainer.querySelector('.input-submit');
+          assert.equal(inputSubmit.getAttribute('type'), 'submit');
+          assert.equal(inputSubmit.getAttribute('value'), 'anmelden');
           done();
         });
     });
