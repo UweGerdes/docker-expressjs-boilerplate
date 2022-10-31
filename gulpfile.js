02d69fb90cf11d0c0d511ea8dc7af28707c34eda
Uwe Gerdes <entwicklung@uwegerdes.de>
2022-10-31 19:21:41 +0100

    reading gulp start list ordered

--- gulpfile.js
+++ gulpfile.js
@@ -40,9 +40,10 @@ if (!process.env.NODE_ENV) {
  */
 Object.keys(config.gulp.start[process.env.NODE_ENV])
   .forEach((group) => {
-    const myTasks = Object.keys(tasks)
-      .filter(key => config.gulp.start[process.env.NODE_ENV][group].includes(key))
+    const myTasks = config.gulp.start[process.env.NODE_ENV][group]
+      .filter(key => Object.keys(tasks).includes(key))
       .reduce((obj, key) => {
+        console.log('start ', group, key);
         return {
           ...obj,
           [key]: tasks[key]
