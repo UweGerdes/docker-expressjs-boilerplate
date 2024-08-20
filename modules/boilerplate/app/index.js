1047b6dd7b1ee463682ed15f64e51e5171988f86
Uwe Gerdes <entwicklung@uwegerdes.de>
2024-08-20 20:58:40 +0200

    module boilerplate gulp server start and  changed tasks

--- modules/boilerplate/app/index.js
+++ modules/boilerplate/app/index.js
@@ -6,4 +6,29 @@
 
 'use strict';
 
+const chalk = require('chalk'),
+  dateFormat = require('dateformat'),
+  express = require('express'),
+  morgan = require('morgan'),
+  config = require('../../../lib/config');
+
+const app = express();
+const port = 3000;
+
+if (config.server.verbose) {
+  morgan.token('time', () => {
+    return dateFormat(new Date(), 'HH:MM:ss');
+  });
+  app.use(morgan('[' + chalk.gray(':time') + '] ' +
+    'boilerplate app server: :method :status :url :res[content-length] Bytes - :response-time ms'));
+}
+
+app.get('/', (req, res) => {
+  res.send('Hello other World!\n');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
 console.log('loaded: modules/boilerplate/app/index.js');
--- modules/boilerplate/gulp/server.js
+++ modules/boilerplate/gulp/server.js
@@ -5,23 +5,55 @@
 
 'use strict';
 
-const path = require('path'),
+delete require.cache[require.resolve('gulp-develop-server')];
+
+const gulp = require('gulp'),
+  server = require('gulp-develop-server'),
+  livereload = require('gulp-livereload'),
+  path = require('path'),
   config = require('../../../lib/config'),
-  log = require('../../../lib/log');
+  log = require('../../../lib/log'),
+  lint = require('../../../gulp/lint');
 
 const tasks = {
   /**
-   * test task
+   * boilerplate sample server start task
    *
-   * @function boilerplate-server
+   * @function boilerplate-server-start
    * @param {function} callback - gulp callback to signal end of task
    */
   /* c8 ignore next 4 */
-  'boilerplate-server': (callback) => {
-    log.info('boilerplate-server called ' + Object.keys(config));
+  'boilerplate-server-start': (callback) => {
+    log.info('boilerplate-server-start called ' + Object.keys(config));
     log.info(path.join(__dirname, config.modules.boilerplate.app.server));
-    callback();
-  }
+    server.listen(
+      {
+        path: path.join(__dirname, config.modules.boilerplate.app.server),
+        env: { VERBOSE: true, FORCE_COLOR: 1 },
+        delay: 9000
+      },
+      callback
+    );
+  },
+  /**
+   * boilerplate sample server changed (restart) task
+   *
+   * @function boilerplate-server-changed
+   * @param {function} callback - gulp callback to signal end of task
+   */
+  /* c8 ignore next 4 */
+  'boilerplate-server-changed': gulp.series(
+    lint.eslint,
+    function serverChanged(callback) {
+      log.info('boilerplate-server-changed called ' + Object.keys(config));
+      server.changed((error) => {
+        if (!error) {
+          livereload.changed({ path: '/', quiet: false });
+        }
+        callback();
+      });
+    }
+  )
 };
 
 module.exports = tasks;
